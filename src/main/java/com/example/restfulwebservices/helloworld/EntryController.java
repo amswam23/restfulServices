3306b7c1749372b40bc7e912b8d4e564a39c5b4f
amswam23 <96985549+amswam23@users.noreply.github.com>
2024-08-26 14:31:25 -0400

    Update EntryController.java
    checking

--- src/main/java/com/example/restfulwebservices/helloworld/EntryController.java
+++ src/main/java/com/example/restfulwebservices/helloworld/EntryController.java
@@ -8,7 +8,7 @@ public class EntryController {
     @GetMapping(path= "/home")
     public String hello() {
         System.out.print("Hello! Swami"); 
-        return "Hello world Swami !!";
+        return "Hello world Swami nathan!!";
 
     }
 }
